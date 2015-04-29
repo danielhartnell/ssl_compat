d4b557b3de0a66d4992960c71aa019506182b4b2
Matt Wobensmith <mwobensmith@mozilla.com>
2015-04-28 17:46:09 -0700

    Cleaned up bash file for better readability

--- bash_test.sh
+++ bash_test.sh
@@ -9,13 +9,13 @@
 for i in "$@"
 do
 case $i in
-    -u=*|--build_url=*)
+    -u=*)
     test_build_url="${i#*=}"
     ;;
-    -s=*|--source_url=*)
+    -s=*)
     source="${i#*=}"
     ;;
-    -d=*|--desc=*)
+    -d=*)
     description="${i#*=}"
     ;;
     -h)
@@ -26,17 +26,16 @@ case $i in
 esac
 done
 
-
 # check arguments
 if [ -n "${help}" ]
 then
     echo $'\n'Usage:
-    echo -d description of test run - optional
     echo -s name of source list
     echo $'\t'options:
     echo $'\t'smoke, test, pulse, google, combined
     echo $'\t'OR custom URL to source list
-    echo -u URL of Firefox DMG to download and test $'\n'
+    echo -u URL of Firefox DMG to download and test 
+    echo -d description of test run in quotes \(optional\) $'\n'
     exit
 fi
 
@@ -56,7 +55,6 @@ if [ -z ${description+x} ]
 then
     description="Generic test description"
 fi
-
 
 # source URL list lengths:
 # smoke 100
@@ -122,9 +120,6 @@ case $test_build_url in
 ;;
 esac
 
-mkdir $timestamp
-TEST_DIR=$DIR"/"$timestamp
-
 curl -# -C - -o 'LatestFirefox.dmg' $test_build_url
 open 'LatestFirefox.dmg'
 sleep 15
@@ -132,10 +127,16 @@ sleep 15
 # move Firefox build from dmg to local folder
 cp -rf "/Volumes/"$volume"/"$app_name .
 
-# create temp log file
+# create test folder and temp log file
+mkdir $timestamp
+TEST_DIR=$DIR"/"$timestamp
 cd $TEST_DIR
+
+# grab number of sites in source file
 temp=( $( wc -l $DIR"/sources/"$url_source ) )
 num_sites="${temp[0]}"
+
+# dump metadata to log file
 metadata="Metadata : "$timestamp" : "$source_name" "$num_sites" sites : "$description
 test_title=$run_date" : "$source_name" "$num_sites" sites : "$description
 echo $metadata$'\n'Build : $test_build_url > log.txt
@@ -156,14 +157,14 @@ if [ "$?" -ne "0" ]; then
     $XPC_SHELL -g $greDir -a $appDir -s $DIR"/sslScan.js" $DIR"/sources/"$url_source $TEST_DIR"/"errors.txt
 fi
 
-echo "TEST 1 OVER"
+echo "First pass on test build of Firefox complete"
 cut -f1,1 -d " " errors.txt > error_urls.txt
 sleep 2
 
 
 # run it again to remove noise
 $XPC_SHELL -g $greDir -a $appDir -s $DIR"/sslScan.js" $TEST_DIR"/"error_urls.txt $TEST_DIR"/"errors.txt
-echo "TEST 2 OVER"
+echo "Second pass on test build of Firefox complete"
 
 sort -u errors.txt > final_errors.txt
 cut -f1,1 -d " " final_errors.txt > error_urls.txt
@@ -182,7 +183,7 @@ if [ "$?" -ne "0" ]; then
     XPC_SHELL=$DIR"/xpcshell_new"
     $XPC_SHELL -g $greDir -a $appDir -s $DIR"/sslScan.js" $TEST_DIR"/"error_urls.txt $TEST_DIR"/"_control_errors.txt
 fi
-echo "TEST 3 OVER"
+echo "Tests on release Firefox complete."
 
 # sort error output
 sort -u _control_errors.txt > control_errors.txt
@@ -191,6 +192,7 @@ sort -u _control_errors.txt > control_errors.txt
 diff -U 0 final_errors.txt control_errors.txt  | grep "^[+-]" | LC_ALL='C' sort > _error_diff.txt
 sleep 3
 
+# merge contents of log file with diff file
 cat log.txt _error_diff.txt > error_diff.txt
 sleep 2
 
@@ -198,7 +200,6 @@ sleep 2
 arg_string='s/METADATA/'$test_title'/g'
 sed -e "$arg_string" $DIR"/"report_template.htm > "$TEST_DIR/"index.htm
 sleep 2
-
 
 # remove some test files
 rm -rf errors.txt
@@ -210,4 +211,5 @@ rm -rf $DIR"/LatestFirefox.dmg"
 rm -rf $DIR"/"$app_name
 
 open index.htm
+
 
