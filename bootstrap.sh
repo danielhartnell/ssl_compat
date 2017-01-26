5295e4d4683e5267e1e73eae5caa2b18d71c05ee
Matt Wobensmith <mwobensmith@mozilla.com>
2017-01-26 13:23:50 -0800

    Bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 sudo apt-get install libgtk-3-0
 sudo apt-get install libxt6
 sudo apt-get install libasound2
-sudo apt-get python
-sudo apt-get python-dev
+sudo apt-get install python
+sudo apt-get install python-dev
 sudo apt-get install virtualenv
-sudo apt-get gcc
-git clone https://github.com/mwobensmith/tls-canary
+sudo apt-get install gcc
+git clone https://github.com/mwobensmith/tls-canary ../tls-canary
