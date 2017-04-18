4d70c5119e500547d7c234b563bacc09be071daa
Matt Wobensmith <mwobensmith@mozilla.com>
2017-04-18 09:04:10 -0700

    Test

--- temp_onecrl.py
+++ temp_onecrl.py
@@ -3,10 +3,11 @@ import os
 import sys
 import time
 import urllib2
+from shutil import rmtree
 
 
 
-workdir=""
+workdir="./test"
 
 one_crl_environ="prod"
 
@@ -18,24 +19,29 @@ for location in locations:
     if os.path.isfile(candidate):
         candidates.append(candidate)
 go_orig = candidates[0]
-go_orig_dir = os.path.dirname(go_orig)
-go_path = os.readlink(go_orig)
 go_real_path = os.path.realpath(go_orig)
 go_src_path = os.path.normpath(go_real_path+"../../../src")
 
+temp_dir = go_src_path+"/github.com"
 
-print (go_src_path)
+if not os.path.exists(temp_dir):
+	os.mkdir(temp_dir)
+	os.mkdir(temp_dir+"/mozmark")
+	os.mkdir(temp_dir+"/mozmark/OneCRL-Tools")
 
 
-#os.mkdir(go_src_path+"/github.com")
-#os.mkdir(go_src_path+"/github.com/mozmark")
+# put OneCRL code into this directory
+git_src_path = temp_dir+"/mozmark/OneCRL-Tools"
+cmd1 = "git clone https://github.com/mozmark/OneCRL-Tools " + git_src_path
+os.system(cmd1)
+
+# obtain OneCRL list and write to disk
+one_crl_app = git_src_path+"/oneCRL2RevocationsTxt/main.go"
+cmd2 = "go run "+one_crl_app+ " "+one_crl_environ+" > "+workdir+"/revocations.txt"
+os.system(cmd2)
+
+# remove temporary directory
+rmtree(temp_dir)
 
 
 print ("done")
-# put OneCRL code into this directory
-
-
-# obtain OneCRL list and write to disk
-
-
-# remove directory
